5b8fe062f691410dbe015c0106b915cb6abea8de
Richard Browning <mi888uzl@outlook.com>
2022-07-23 04:06:11 +0800

    button event listener

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { Suspense } from "react";
+import React, { Component, Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, View } from "@react-three/drei";
 import { Container } from "react-bootstrap";
+import $ from 'jquery'
 
 import Radio from "./Radio"
 import Kick from "./UserAvatarAnimation.js";
@@ -12,25 +13,47 @@ import Ninja from "./NinjaIdle";
 
 import "./styles.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { render } from "react-dom";
 
-export default function App() {
-  return (
-    <div className="row h-100" style={{}}>
-      <div className="col-2 btn-group-vertical" id="buttonContainer">
-        <Radio/>
+export default class App extends Component {
+    jQuerycode = () => {
+      $("#idleButton").on('click',function(){
+          console.log("idle")
+      });
+      $("#readyButton").on('click',function(){
+          console.log("ready")
+      });
+      $("#ninjaButton").on('click',function(){
+          console.log("ninja")
+      });
+      $("#happyButton").on('click',function(){
+          console.log("happy")
+      });
+  } 
+    componentDidMount(){
+        this.jQuerycode()
+    }
+  render(){
+    return (
+      <div className="row h-100" style={{}}>
+        <div className="col-2 btn-group-vertical" id="buttonContainer">
+          <Radio/>
+        </div>
+        <div id="modelContainer" className="h-100 col-8 d-inline-block">
+          <Canvas  camera={{position: [-3, 6, 20], fov: 5.3}}>
+            <OrbitControls />
+            <ambientLight intensity={0.6} />
+            <directionalLight intensity={0.5} />
+            <Suspense fallback={null}>
+              <Ninja/>
+            </Suspense>
+          </Canvas>
+        </div>
       </div>
-      <div id="modelContainer" className="h-100 col-8 d-inline-block">
-        <Canvas  camera={{position: [-3, 6, 20], fov: 5.3}}>
-          <OrbitControls />
-          <ambientLight intensity={0.6} />
-          <directionalLight intensity={0.5} />
-          <Suspense fallback={null}>
-            <Ninja />
-          </Suspense>
-        </Canvas>
-      </div>
-    </div>
-  );
+    )
+
+  }
+
 }
 
 
--- src/Radio.js
+++ src/Radio.js
@@ -1,28 +1,10 @@
 import Button from 'react-bootstrap/Button';
-import $ from 'jquery'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Component } from 'react';
 
 class actionButtons extends Component{
-    jQuerycode = () => {
-        $("#idleButton").click(function(){
-            console.log("idle")
-        });
-        $("#readyButton").click(function(){
-            console.log("ready")
-        });
-        $("#ninjaButton").click(function(){
-            console.log("ninja")
-        });
-        $("#happyButton").click(function(){
-            console.log("happy")
-        });
-    } 
 
-    componentDidMount(){
-        this.jQuerycode()
-    }
     render(){
         return (
             <> 
