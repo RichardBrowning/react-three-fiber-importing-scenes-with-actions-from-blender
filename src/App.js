370cefa27e764347ef8139ea3b21b47d50cf0ed1
RichardBrowning <mi888uzl@outlook.com>
2022-07-30 03:30:23 +0800

    rebuild on button change

--- src/App.js
+++ src/App.js
@@ -1,39 +1,38 @@
-import React, { Component, createRef, Suspense, useEffect, useRef } from "react";
+import React, { Component, createRef, Suspense, useEffect, useRef, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import $ from 'jquery'
 
 import Radio from "./Radio"
-import Kick from "./UserAvatarAnimation.js";
-import One from "./Idle";
-import Ready from "./Ready";
-import Happy from "./HappyIdle";
-import Ninja from "./NinjaIdle";
+import Kick from "./gltfJSX/UserAvatarAnimation";
+import One from "./gltfJSX/Idle";
+import Ready from "./gltfJSX/Ready";
+import Happy from "./gltfJSX/HappyIdle";
+import Ninja from "./gltfJSX/NinjaIdle";
 
 import "./styles.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { render } from "react-dom";
 
 export default function App() {
-    const child = useRef()
-    const handleClick = () =>{
-        child.current.getAlert()
-    }
+    const [actionName, setActionName] = useState('ninyaIdle');
 
     const jQuerycode = () => {
       $("#idleButton").on('click',function(){
-        console.log("idle")
-        child.current.changeAnimationHandler();
-          
+          console.log("idle")
+          setActionName('oneIdle')
       });
       $("#readyButton").on('click',function(){
           console.log("ready")
+          setActionName('readyIdle')
       });
       $("#ninjaButton").on('click',function(){
           console.log("ninja")
+          setActionName('ninyaIdle')
       });
       $("#happyButton").on('click',function(){
           console.log("happy")
+          setActionName('happyIdle')
       });
   } 
     useEffect(
@@ -45,13 +44,13 @@ export default function App() {
         <div className="col-2 btn-group-vertical" id="buttonContainer">
           <Radio/>
         </div>
-        <div id="modelContainer" className="h-100 col-8 d-inline-block">
+        <div id="modelContainer" className="h-100 col-10 d-inline-block">
           <Canvas  camera={{position: [-3, 6, 20], fov: 5.3}}>
             <OrbitControls />
             <ambientLight intensity={0.6} />
             <directionalLight intensity={0.5} />
             <Suspense fallback={null}>
-              <Ninja ref={child} />
+              <Ninja action={actionName} />
             </Suspense>
           </Canvas>
           
--- src/Radio.js
+++ src/Radio.js
@@ -2,16 +2,17 @@ import Button from 'react-bootstrap/Button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Component } from 'react';
+import "./styles.css";
 
 class actionButtons extends Component{
 
     render(){
         return (
             <> 
-                <Button id='idleButton' >Idle</Button>
-                <Button id='readyButton' >Ready</Button>
-                <Button id='ninjaButton' >Ninja</Button>
-                <Button id='happyButton' >Happy</Button>
+                <Button className='actionButton' id='idleButton' >Idle</Button>
+                <Button className='actionButton' id='readyButton' >Ready</Button>
+                <Button className='actionButton' id='ninjaButton' >Ninja</Button>
+                <Button className='actionButton' id='happyButton' >Happy</Button>
                 {/*<Button variant="light">Light</Button>{' '}
                 <Button variant="dark">Dark</Button> <Button variant="link">Link</Button>*/}
             </>
